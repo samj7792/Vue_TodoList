2cc94f82d49080daaba617b4e9e5f0364a26d684
Sam Jackson <samuelsjackson7@gmail.com>
2020-08-18 11:48:23 -0400

    Vue Todo List

---
+++ src/components/ToDos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- todo in v-for can be whatever, todos pertains to the prop -->
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <ToDoItem v-bind:todo="todo" @del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem";
+
+export default {
+  name: "Todos",
+  components: { ToDoItem },
+  props: ["todos"],
+};
+</script>
+
+<style scoped>
+</style>
